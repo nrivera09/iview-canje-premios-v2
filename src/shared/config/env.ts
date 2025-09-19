a2101ff7387abfeae2902b4c760b0dfa05c6d6ab
nrivera09 <nrivera@acity.com.pe>
2025-09-18 23:03:08 -0500

    avance
    avance

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -1,6 +1,7 @@
+// /config/env.ts
 export const ENV = {
-  API_BASE_URL: 'https://dev-api-iview.acity.com.pe/api/',
-  API_BASE_URL_V1: 'https://dev-api-canje-regalo.acity.com.pe/api/',
-  REACT_APP_ACITY_SOCKET: 'https://dev-api-canje-regalo.acity.com.pe/',
-  API_IMG64: 'https://lab-api-assets-iview.acity.com.pe/',
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL ?? '',
+  API_BASE_URL_V1: process.env.REACT_APP_API_BASE_URL_V1 ?? '',
+  REACT_APP_ACITY_SOCKET: process.env.REACT_APP_ACITY_SOCKET ?? '',
+  API_IMG64: process.env.REACT_APP_API_IMG64 ?? '',
 };
